17b12c74dcb827b6da9de6d2bda7dac3ec80f179
capeterson <capeterson@89253570-fd12-4709-a508-8497ac4b1c7b>
2009-12-28 04:00:26 +0000

    Created tag for 3.05 release candidate.
    git-svn-id: svn://svn.code.sf.net/p/tpabbrevia/code@237 89253570-fd12-4709-a508-8497ac4b1c7b

---
+++ tags/3.05 RC/tests/AbFloppySpanTests.pas
@@ -0,0 +1,272 @@
+(* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is TurboPower Abbrevia
+ *
+ * The Initial Developer of the Original Code is
+ * Robert Love
+ *
+ * Portions created by the Initial Developer are Copyright (C) 1997-2002
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK ***** *)
+
+unit AbFloppySpanTests;
+
+{ TODO: Refactor these tests so they work without a floppy drive (VFD doesn't
+  work on Vista x64).  I'm thinking add a new TStream descendant that we can
+  make fail in predictable ways and at predictable sizes.  The archive classes
+  would gain a new "OpenFileStream" function that we can override to return
+  that stream.  Zips would be the only ones that use TAbSpanStream, so we can
+  drop its limitations for the other types. }
+
+{$I AbDefine.inc}
+
+interface
+
+uses
+// Note: The Floppy Span tests are designed to be platform specific
+
+  Windows,
+  TestFrameWork,
+  AbArcTyp, AbUtils,
+  AbTestFramework;
+
+type
+  TAbFloppySpanTests = class(TabTestCase)
+  protected
+    WinDir : string;
+    HandleWriteFailure1Test : Boolean;
+    procedure SetUp; override;
+    procedure TearDown; override;
+    //Events for HandleWriteFailure1
+    procedure HandleWriteFailure1ProgressEvent(Sender : TObject; Item : TAbArchiveItem; Progress : Byte; var Abort : Boolean);
+
+    //Events for CheckFreeForAV
+    procedure CheckFreeForAVFailureEvent(Sender : TObject; Item : TAbArchiveItem;
+      ProcessType : TAbProcessType; ErrorClass : TAbErrorClass; ErrorCode : Integer);
+    procedure CheckFreeForAVItemProgressEvent(Sender : TObject; Item : TAbArchiveItem; Progress : Byte; var Abort : Boolean);
+
+  published
+    procedure CreateBasicSpan;
+    procedure VerifyBasicSpan;
+    procedure CheckFreeForAV;
+    procedure HandleWriteProtectedMedia;
+    procedure HandleWriteFailure1;
+    {$IFDEF WINZIPTESTS}
+    procedure WinzipExtractTest;
+    {$ENDIF}
+  end;
+
+implementation
+
+uses
+  Classes, Controls, Dialogs, SysUtils,
+  AbUnzper, AbZipper;
+
+{ -------------------------------------------------------------------------- }
+procedure TAbFloppySpanTests.SetUp;
+begin
+  inherited;
+  // Get directory windows is installed to
+  SetLength(WinDir, MAX_PATH);
+  SetLength(WinDir, GetWindowsDirectory(PChar(WinDir), MAX_PATH));
+  WinDir := AbAddBackSlash(WinDir);
+end;
+{ -------------------------------------------------------------------------- }
+procedure TAbFloppySpanTests.VerifyBasicSpan;
+var
+  UnZip : TAbUnZipper;
+  mStream : TMemoryStream;
+  I : Integer;
+  TestFile : string;
+begin
+  // Create a Basic Span by zipping all .EXE Files in C:\WINDOWS\
+  // On My(Robert Love) Machine this takes two disks to do.
+
+  if MessageDlg('This test requires the TESTSPAN.ZIP created in the '+#13+#10+'"CreateBasicSpan" test.  Please Insert Disk #1'+#13+#10+''+#13+#10+'Pressing Cancel will terminate this test.', mtInformation, [mbOK,mbCancel], 0) = mrCancel then
+    Fail('Test Aborted');
+
+  UnZip := TAbUnZipper.create(nil);
+  mStream := TMemoryStream.Create;
+  try
+    UnZip.BaseDirectory := GetTestTempDir;
+    CheckFileExists('A:\SPANTEST.ZIP');
+    UnZip.FileName := 'A:\SPANTEST.ZIP';
+    Check(Unzip.Count > 0, 'Archive A:\SPANTEST.ZIP is empty');
+    for I := 0 to Unzip.Count -1 do begin
+      testFile := WinDir + ExtractFileName(UnZip.Items[I].FileName);
+      // Make sure file exist to compare to.
+      CheckFileExists(TestFile);
+      // Extract File in Span to Memory
+      UnZip.ExtractToStream(UnZip.Items[I].FileName, mStream);
+      // Open the Existing File in Read Only Mode.
+      CheckFileMatchesStream(TestFile, mStream, 'Test File: ' + TestFile + ' did not match archive');
+      mStream.SetSize(0);
+    end;
+  finally
+    UnZip.Free;
+    mStream.Free;
+  end;
+end;
+{ -------------------------------------------------------------------------- }
+procedure TAbFloppySpanTests.CreateBasicSpan;
+var
+  Zip : TAbZipper;
+begin
+  // UnZips the Basic Span that was created by zipping all .EXE Files in C:\WINDOWS\
+  // Compares each file byte by byte to original.
+  if MessageDlg('Insert Disk #1 to create A:\TESTSPAN.ZIP'+#13+#10+''+#13+#10+'Pressing Cancel Abort Test', mtWarning, [mbOK,mbCancel], 0) = mrCancel then
+    Fail('User Aborted Test');
+  Zip := TAbZipper.create(nil);
+  try
+    Zip.BaseDirectory := WinDir;
+    Zip.FileName := 'A:\SPANTEST.ZIP';
+    Zip.AddFiles('*.EXE', faAnyFile);
+    Zip.Save;
+  finally
+    Zip.Free;
+  end;
+end;
+{ -------------------------------------------------------------------------- }
+{$IFDEF WINZIPTESTS}
+procedure TAbFloppySpanTests.WinzipExtractTest;
+var
+  ExtractTo : string;
+  FileList : TStringList;
+  I : integer;
+begin
+  // This test will use the Winzip command line utility to determine if the
+  // file created in CreateBasicSpan can be extracted.
+  // This is a good comptability routine.
+
+  if MessageDlg('This test requires the TESTSPAN.ZIP created in the '+#13+#10+'"CreateBasicSpan" test. '+ #13#10 + 'It also requires WinZIP Command Line Utility. '+#13#10#13#10 +'Please Insert LAST Disk of span set.'+#13+#10+''+#13+#10+'Pressing Cancel will terminate this test.', mtInformation, [mbOK,mbCancel], 0) = mrCancel then
+    Fail('Test Aborted');
+
+  ExtractTo := TestTempDir + 'WZSpan\';
+  DelTree(ExtractTo);
+  Check(DirectoryExists(extractTo), 'DelTree() was just called it Directory should be deleted');
+  CreateDir(ExtractTo);
+  ExecuteAndWait(UnWinZip, 'A:\SPANTEST.ZIP ' + ExtractTo);
+
+  // Files have now been extracted Time to test.
+  FileList := FilesInDirectory(ExtractTo);
+  try
+    // Make sure files where extracted
+    Check(Filelist.Count > 0, 'Unable to find any extract files');
+    for I := 0 to FileList.Count -1 do
+      CheckFilesMatch(WinDir + FileList[I], ExtractTo + FileList[I], FileList[I] + ' did not match master file');
+  finally
+    FileList.free;
+  end;
+end;
+{$ENDIF}
+{ -------------------------------------------------------------------------- }
+procedure TAbFloppySpanTests.TearDown;
+begin
+  inherited;
+end;
+{ -------------------------------------------------------------------------- }
+procedure TAbFloppySpanTests.CheckFreeForAV;
+var
+  FUnZipper : TAbUnZipper;
+begin
+  //[ 785269 ] Access violation on free
+  ShowMessage('Insert Floppy Disk with FTest.zip on it');
+  FUnZipper := TAbUnZipper.Create(nil);
+  with FUnZipper do
+    try
+      ForceType := True;
+      ArchiveType := atZip;
+      BaseDirectory := GetTestTempDir;
+
+      OnProcessItemFailure := CheckFreeForAVFailureEvent;
+//      OnRequestLastDisk := RequestLastDisk;
+//      OnRequestNthDisk := RequestNthDisk;
+//      OnArchiveProgress := Progress;
+      OnArchiveItemProgress := CheckFreeForAVItemProgressEvent;
+      FileName:='A:\Ftest.zip';
+      ExtractFiles('*.*');
+    finally
+      Free;
+    end;
+end;
+{ -------------------------------------------------------------------------- }
+procedure TAbFloppySpanTests.CheckFreeForAVFailureEvent(Sender: TObject;
+  Item: TAbArchiveItem; ProcessType: TAbProcessType;
+  ErrorClass: TAbErrorClass; ErrorCode: Integer);
+begin
+  // Do Nothing
+end;
+{ -------------------------------------------------------------------------- }
+procedure TAbFloppySpanTests.CheckFreeForAVItemProgressEvent(
+  Sender: TObject; Item: TAbArchiveItem; Progress: Byte;
+  var Abort: Boolean);
+begin
+  Abort := True;
+end;
+{ -------------------------------------------------------------------------- }
+procedure TAbFloppySpanTests.HandleWriteProtectedMedia;
+var
+  Zip : TAbZipper;
+begin
+  ExpectedException := EFCreateError;
+  ShowMessage('Insert Blank Write Protected Disk in to Drive A');
+  Zip := TAbZipper.Create(nil);
+  try
+    Zip.BaseDirectory := GetTestFileDir;
+    Zip.FileName := 'A:\SPANTEST.ZIP';
+    Zip.AddFiles('MPL-1_1.txt',faAnyFile);
+    Zip.Save;
+  finally
+    Zip.Free;
+  end;
+end;
+{ -------------------------------------------------------------------------- }
+procedure TAbFloppySpanTests.HandleWriteFailure1ProgressEvent(
+  Sender: TObject; Item: TAbArchiveItem; Progress: Byte;
+  var Abort: Boolean);
+begin
+  if Progress > 50 then
+    ShowMessage('Take Disk out of drive to simulate failure');
+end;
+{ -------------------------------------------------------------------------- }
+procedure TAbFloppySpanTests.HandleWriteFailure1;
+var
+  Zip : TAbZipper;
+begin
+  //[ 785249 ] ProcessItemFailure not called
+  ExpectedException := EFOpenError;
+  ShowMessage('Insert Blank Formated Disk in to Drive A');
+  Zip := TAbZipper.Create(nil);
+  try
+    Zip.BaseDirectory := GetTestFileDir;
+    Zip.OnArchiveItemProgress := HandleWriteFailure1ProgressEvent;
+    Zip.FileName := 'A:\SPANTEST.ZIP';
+    Zip.AddFiles('MPL-1_1.txt', faAnyFile);
+    Zip.Save;
+  finally
+    Zip.Free;
+  end;
+end;
+{ -------------------------------------------------------------------------- }
+
+initialization
+
+  TestFramework.RegisterTest('Abbrevia.Floppy Spanning Suite',
+    TAbFloppySpanTests.Suite);
+
+end.
+
---
+++ tags/3.05 RC/tests/AbZipKitTests.pas
@@ -0,0 +1,295 @@
+(* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is TurboPower Abbrevia
+ *
+ * The Initial Developer of the Original Code is
+ * Robert Love
+ *
+ * Portions created by the Initial Developer are Copyright (C) 1997-2002
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK ***** *)
+
+unit AbZipKitTests;
+
+{$I AbDefine.inc}
+
+interface
+
+uses
+  AbTestFrameWork, AbZipKit;
+
+type
+  TAbZipKitTests = class(TAbCompTestCase)
+  private
+    Component : TAbZipKit;
+
+  protected
+    procedure SetUp; override;
+    procedure TearDown; override;
+
+  published
+    procedure TestDefaultStreaming;
+    procedure TestComponentLinks;
+    procedure TestAddThenExtract;
+    procedure TestTaggedFiles;
+    procedure FreshenTest;
+    procedure FreshenBaseDir;
+    procedure TestComment;
+  end;
+
+implementation
+
+uses
+  {$IFDEF MSWINDOWS}
+  Windows,
+  {$ENDIF}
+  Classes, SysUtils,
+  TestFrameWork,
+  AbArcTyp, AbMeter, AbZipTyp;
+
+{ TAbZipKitTests }
+
+procedure TAbZipKitTests.FreshenBaseDir;
+  // Test Freshen without setting the Base Directory
+  // SF.NET Tracker [ 892830 ] DiskFileName is not set correctly
+var
+  TestFile : string;
+  SL : TStringList;
+  MS : TMemoryStream;
+begin
+  TestFile := TestTempDir + 'freshenBaseDir.zip';
+  if FileExists(TestFile) then
+    DeleteFile(TestFile);
+
+  Component.StoreOptions := Component.StoreOptions + [soRecurse,soFreshen];
+  Component.FileName := TestFile;
+  Component.DeflationOption := doMaximum;
+
+  // Create Files to add
+
+  // Create 3 Text Files to add to archive.
+  SL := TStringList.Create;
+  try
+    SL.Add('Test File');
+
+    SL.SaveToFile(TestTempDir + 'Freshen1base.fsh');
+    SL.SaveToFile(TestTempDir + 'Freshen2base.fsh');
+    SL.SaveToFile(TestTempDir + 'Freshen3base.fsh');
+    Component.AddFiles(TestTempDir + 'Freshen1base.fsh', 0);
+    Component.AddFiles(TestTempDir + 'Freshen2base.fsh', 0);
+    Component.AddFiles(TestTempDir + 'Freshen3base.fsh', 0);
+
+    Component.CloseArchive;
+
+    // Modify the 2nd File
+    SL.Add('Modification');
+    SL.SaveToFile(TestTempDir + 'Freshen2base.fsh');
+
+    // Freshen the archive
+    Component.FileName := TestFile;
+    Component.DeflationOption := doMaximum;
+    Component.StoreOptions := Component.StoreOptions + [soRecurse, soFreshen];
+    Component.AddFiles(TestTempDir + 'Freshen1base.fsh', 0);
+    Component.AddFiles(TestTempDir + 'Freshen2base.fsh', 0);
+    Component.AddFiles(TestTempDir + 'Freshen3base.fsh', 0);
+    Component.Save;
+
+    // Make sure modified file and archive value matches
+    MS := TMemoryStream.create;
+    try
+      Component.ExtractToStream(Component.Items[1].FileName, MS);
+      CheckFileMatchesStream(TestTempDir + 'Freshen2base.fsh', MS,
+        'Freshened file on disk did not match archived value');
+    finally
+      MS.Free;
+    end;
+  finally
+    SL.Free;
+    DeleteFile(TestTempDir + 'Freshen1base.fsh');
+    DeleteFile(TestTempDir + 'Freshen2base.fsh');
+    DeleteFile(TestTempDir + 'Freshen3base.fsh');
+    DeleteFile(TestFile);
+  end;
+end;
+
+procedure TAbZipKitTests.FreshenTest;
+  // [887909] soFreshen isn't working
+var
+  SL : TStringList;
+  MS : TMemoryStream;
+begin
+  // Create 3 Text Files to add to archive.
+  SL := TStringList.Create;
+  try
+    SL.Add('Test File');
+
+    SL.SaveToFile(TestTempDir + 'Freshen1.fsh');
+    SL.SaveToFile(TestTempDir + 'Freshen2.fsh');
+    SL.SaveToFile(TestTempDir + 'Freshen3.fsh');
+
+    if FileExists(TestTempDir + 'Freshen.zip') then
+      DeleteFile(TestTempDir + 'Freshen.zip');
+
+    Component.FileName := TestTempDir + 'Freshen.zip';
+    Component.BaseDirectory := TestTempDir;
+    Component.DeflationOption := doMaximum;
+    Component.StoreOptions := Component.StoreOptions + [soRecurse, soFreshen];
+    Component.AddFiles('*.fsh',0);
+    Component.Save;
+    Component.CloseArchive;
+
+    // Modify the 2nd File
+    SL.Add('Modification');
+    SL.SaveToFile(TestTempDir + 'Freshen2.fsh');
+
+    // Freshen the archive
+    Component.FileName := TestTempDir + 'Freshen.zip';
+    Component.BaseDirectory := TestTempDir;
+    Component.DeflationOption := doMaximum;
+    Component.StoreOptions := Component.StoreOptions + [soRecurse, soFreshen];
+    Component.AddFiles('*.fsh',0);
+    Component.Save;
+
+    // Make sure modified file and archive value matches
+    MS := TMemoryStream.create;
+    try
+      Component.ExtractToStream('Freshen2.fsh',MS);
+      CheckFileMatchesStream(TestTempDir + 'Freshen2.fsh', MS,
+        'Freshened file on disk did not match archived value');
+    finally
+      MS.Free;
+    end;
+  finally
+    SL.Free;
+    DeleteFile(TestTempDir + 'Freshen1.fsh');
+    DeleteFile(TestTempDir + 'Freshen2.fsh');
+    DeleteFile(TestTempDir + 'Freshen3.fsh');
+    DeleteFile(TestTempDir + 'Freshen.zip');
+  end;
+end;
+
+procedure TAbZipKitTests.SetUp;
+begin
+  inherited;
+  Component := TAbZipKit.Create(TestForm);
+end;
+
+procedure TAbZipKitTests.TearDown;
+begin
+  inherited;
+end;
+
+procedure TAbZipKitTests.TestAddThenExtract;
+var
+  MS : TMemoryStream;
+  I : Integer;
+begin
+  // [ 785769 ] SF.NET Tracker ID is the Bug this is testing for.
+
+  // This test is designed to add to an archive
+  // Then extract from it without having to close/reopen archive.
+
+  if FileExists(TestTempDir + 'ZKitTest.zip') then
+    DeleteFile(TestTempDir + 'ZKitTest.zip');
+
+  Component.FileName := TestTempDir + 'ZKitTest.zip';
+  Component.BaseDirectory := GetWindowsDir;
+  Component.AddFiles('*.ZIP',faAnyFile);
+  Component.Save;
+  MS := TMemoryStream.Create;
+  try
+    for I := 0 to Component.Count - 1 do begin
+      // Compare uncompressed files to original files
+      Component.ExtractToStream(Component.Items[I].FileName, MS);
+      CheckFileMatchesStream(TestFileDir + ExtractFileName(Component.Items[I].FileName),
+        MS, 'File ' + Component.Items[I].FileName + ' did not match original');
+      MS.Clear;
+    end;
+  finally
+    MS.Free;
+  end;
+end;
+
+procedure TAbZipKitTests.TestComponentLinks;
+begin
+  TestComponentLink(Component, 'ArchiveProgressMeter', TAbMeterLink);
+  TestComponentLink(Component, 'ArchiveSaveProgressMeter', TAbMeterLink);
+  TestComponentLink(Component, 'ItemProgressMeter', TAbMeterLink);
+end;
+
+procedure TAbZipKitTests.TestDefaultStreaming;
+var
+  CompStr : string;
+  CompTest : TAbZipKit;
+begin
+  RegisterClass(TAbZipKit);
+  CompStr := StreamComponent(Component);
+  CompTest := (UnStreamComponent(CompStr) as TAbZipKit);
+  CompareComponentProps(Component,CompTest);
+  UnRegisterClass(TAbZipKit);
+end;
+
+procedure TAbZipKitTests.TestTaggedFiles;
+  // [ 806077 ] TestTaggedItems fails if called after modifying an archive
+begin
+  DeleteFile(TestTempDir + 'test.zip');
+  Component.FileName := TestTempDir + 'test.zip';
+  Component.AutoSave := True;
+  Component.BaseDirectory := TestFileDir;
+  Component.ClearTags;
+  Component.AddFiles('*.*',0);
+  Component.TagItems('*.*');
+  Check(Component.Count > 0);
+  // TestTaggedItems should not raise an error.
+  Component.TestTaggedItems;
+  Component.CloseArchive;
+end;
+
+procedure TAbZipKitTests.TestComment;
+  // Verify that Delphi 2009 handles zip file comments correctly (fixed in rev 158)
+const
+  SComment: AnsiString = 'Test comment';
+var
+  Zip: TAbZipKit;
+begin
+  DeleteFile(TestTempDir + 'comment.zip');
+  Zip := TAbZipKit.Create(nil);
+  try
+    Zip.FileName := TestTempDir + 'comment.zip';
+    Zip.ZipFileComment := SComment;
+    Zip.BaseDirectory := TestFileDir;
+    Zip.AddFiles('MPL-1_1.txt', 0);
+    Zip.Save;
+  finally
+    Zip.Free;
+  end;
+  Zip := TAbZipKit.Create(nil);
+  try
+    Zip.FileName := TestTempDir + 'comment.zip';
+    CheckEquals(SComment, Zip.ZipFileComment);
+  finally
+    Zip.Free
+  end;
+end;
+
+initialization
+
+  TestFramework.RegisterTest('Abbrevia.Component Level Test Suite',
+    TAbZipKitTests.Suite);
+ 
+end.
+
